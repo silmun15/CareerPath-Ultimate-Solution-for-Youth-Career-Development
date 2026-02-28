a69df4b8787ff9327d0bc5754221191a119ec169
Rafi12234 <shajidulkabir12345@gmail.com>
2026-02-28 20:16:33 +0600

    fixed Dashboard bugs

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-// Jobs
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/{id}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::put('/jobs/{id}', [JobController::class, 'update']);
-Route::delete('/jobs/{id}', [JobController::class, 'destroy']);
-
 // Courses
 Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{id}', [CourseController::class, 'show']);
